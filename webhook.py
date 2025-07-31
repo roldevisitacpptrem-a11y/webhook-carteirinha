dc8f287d8e75948ba45c0a06e934d7fe0fa6a48f
roldevisitacpptrem <roldevisitacpptrem@gmail.com>
2025-07-31 09:22:34 -0300

    Fix PORT for Render deployment

--- webhook.py
+++ webhook.py
@@ -48,5 +48,6 @@ def webhook():
 
     return jsonify({'fulfillmentText': resposta})
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render define essa variável
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
